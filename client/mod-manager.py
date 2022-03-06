b13f578b208a7d410623efb936ed9ba86626a07a
Callum Law <callumlaw1709@outlook.com>
2022-03-07 12:17:27 +1300

    Update flow to upgrade after all y/n decisions have been made rather than after each mod

--- client/mod-manager.py
+++ client/mod-manager.py
@@ -10,7 +10,7 @@ from pathlib import Path
 try:
     import semver
 except ImportError as e:
-    print("Semver has not yet been installed. Do: python3 -m pip install semver")
+    print('Semver has not yet been installed. Do: python3 -m pip install semver')
     exit(1)
 
 API_URL = '<api_url>'
@@ -18,21 +18,15 @@ FABRIC_VERSION_REGEX = r'fabric-loader-\d+\.\d+\.\d+-.+'
 FABRIC_VERSION_REPLACEMENT_REGEX = r'fabric-loader-\d+\.\d+\.\d+-'
 STRIP_LEADING_ZEROES_REGEX=r'^0+(\d+)'
 
-def get_versions(directory: str):
+def get_installed_minecraft_versions(directory: str):
     def version_directory_filter(file: str):
-        return os.path.isdir(f"{directory}/{file}") and (re.match(FABRIC_VERSION_REGEX, file) or os.path.exists(f"{directory}/{file}/server-{file}.jar"))
+        return os.path.isdir(f'{directory}/{file}') and (re.match(FABRIC_VERSION_REGEX, file) or os.path.exists(f'{directory}/{file}/server-{file}.jar'))
 
     return list(set(map(lambda version: re.sub(FABRIC_VERSION_REPLACEMENT_REGEX, "", version), filter(version_directory_filter, os.listdir(directory)))))
 
-def normalize_names(directory: str):
-    [ mods, _ ] = get_installed_mods(directory)
-
-    for mod in mods:
-        os.rename(f"{directory}/{mods[mod].get('file')}", f"{directory}/{mod}.jar")
-
 def parse_fabric_manifest_from_jar(path: str) -> dict:
-    with zipfile.ZipFile(path, "r") as zip_ref:
-        with zip_ref.open("fabric.mod.json") as fabric_manifest:
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        with zip_ref.open('fabric.mod.json') as fabric_manifest:
             return json.loads(fabric_manifest.read())
 
 def get_installed_mods(directory: str) -> dict:
@@ -40,33 +34,42 @@ def get_installed_mods(directory: str) -> dict:
     installed = dict()
 
     for file in os.listdir(directory):
-        if file.endswith(".jar"):
+        if file.endswith('.jar'):
             info = parse_fabric_manifest_from_jar(f"{directory}/{file}")
 
+            #Remove leading zeroes from each portion of the version i.e. 1.01.0 -> 1.1.0
             version = ".".join(map(lambda portion: portion if portion == '0' else re.sub(STRIP_LEADING_ZEROES_REGEX, r'\1', portion), info.get('version').split('.')))
             
             if semver.VersionInfo.isvalid(version):
-                installed[info.get("id")] = { "file": file, "version": version }
+                installed[info.get('id')] = {
+                    'file': file,
+                    'version': version
+                }
             else:
                 non_semver_mods.append(info.get('id'))
 
     return [ installed, non_semver_mods ]
 
-install_dir =  f'{Path.home()}/.minecraft/' if sys.platform == "linux" or sys.platform == "linux2" else f"{os.getenv('APPDATA')}/.minecraft/"
+def download_mod(url: str, filename: str):
+    print(f'Downloading {filename}...')
+    response = requests.get(url, allow_redirects=True)
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+install_dir =  f'{Path.home()}/.minecraft/' if sys.platform == 'linux' or sys.platform == 'linux2' else f'{os.getenv("APPDATA")}/.minecraft/'
 
 if (len(sys.argv) > 1):
     install_dir = sys.argv[1] + '/'
 
-versions_dir = f"{install_dir}versions/"
-mods_dir = f"{install_dir}mods/"
+versions_dir = f'{install_dir}versions/'
+mods_dir = f'{install_dir}mods/'
 
-if (not os.path.exists(install_dir)):
+if (not os.path.exists(mods_dir)):
     print(f"Specified folder {mods_dir} does not exist")
     exit(1)
 
-normalize_names(mods_dir)
-
-versions = get_versions(versions_dir)
+versions = get_installed_minecraft_versions(versions_dir)
 
 if len(versions) == 0:
     print("It doesn't seem like you have fabric installed")
@@ -111,20 +114,18 @@ if len(mods_not_in_repository) != 0:
     for mod in mods_not_in_repository:
         print(f" - {mod}")
 
-up_to_date_versions = data.get("versions")
+up_to_date_versions = data.get('versions')
+mods_to_update = list()
 
 for mod in up_to_date_versions.keys():
     installed_version = installed_mods.get(mod).get('version')
     up_to_date_version = up_to_date_versions.get(mod).get('version')
 
     if(semver.compare(installed_version, up_to_date_version) == -1):
-        user_input = input(f"{mod} is out of date: {installed_version} vs {up_to_date_version}. Would you like to update? (y/n):")
+        user_input = input(f'{mod} is out of date: {installed_version} vs {up_to_date_version}. Would you like to update? (y/n):')
 
-        if(user_input.lower() != 'y'):
-            continue
-            
-        downloaded = requests.get(up_to_date_versions.get(mod).get('downloadUrl'), allow_redirects=True)
+        if(user_input.lower() == 'y'):
+            mods_to_update.append(mod)
 
-        file = open(f"{mods_dir}/{mod}.jar", "wb")
-
-        file.write(downloaded.content)
+for mod in mods_to_update:
+    download_mod(up_to_date_versions.get(mod).get('downloadUrl'), f"{mods_dir}{mod}.jar")
